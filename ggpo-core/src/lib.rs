6fc27adc47fd0c1831e1dedbb988e2a515343c91
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2022-07-19 00:02:43 -0500

    Introduce ringbuf
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- ggpo-core/src/lib.rs
+++ ggpo-core/src/lib.rs
@@ -1,10 +1,22 @@
-pub mod structs;
+mod structs;
+
+pub use structs::*;
+
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let mut buffer: RingBuffer<usize> = RingBuffer::new(10);
+        let (mut producer, mut consumer) = buffer.split();
+        producer.push(1).unwrap();
+        producer.push(2).unwrap();
+        producer.push(3).unwrap();
+
+        assert_eq!(consumer.pop().unwrap(), 1);
+        assert_eq!(consumer.pop().unwrap(), 2);
+        assert_eq!(consumer.pop().unwrap(), 3);
     }
 }
--- ggpo-core/src/structs/mod.rs
+++ ggpo-core/src/structs/mod.rs
@@ -1,3 +1,9 @@
+#[cfg(not(feature = "ringbuf"))]
 mod ring_buffer;
 
+#[cfg(not(feature = "ringbuf"))]
 pub use ring_buffer::*;
+
+
+#[cfg(feature = "ringbuf")]
+pub use ringbuf::*;
